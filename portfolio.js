b6c1642678d301e41fe0de765003ef247dbd9389
Abhishek Sharma <sharmabhishek1410@gmail.com>
2026-06-20 15:15:10 +0530

    Update contact link to relative path

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
 let but = document.querySelector("button")
 but.addEventListener("click", function(){
-    window.open("http://127.0.0.1:5500/Portfolio/contact.html")
+    window.open("contact.html")
 })
